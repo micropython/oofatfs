783cf2d9e8dabbdb5639971b86417624d7cdcbc9
Damien George <damien.p.george@gmail.com>
2016-03-07 13:41:20 +0000

    src: Split out umount functionality from f_mount to new f_umount.

--- src/ff.c
+++ src/ff.c
@@ -3196,7 +3196,7 @@ FRESULT validate (  /* Returns FR_OK or FR_INVALID_OBJECT */
 /*-----------------------------------------------------------------------*/
 
 FRESULT f_mount (
-    FATFS* fs,          /* Pointer to the file system object (NULL:unmount)*/
+    FATFS* fs,          /* Pointer to the file system object to mount */
     const TCHAR* path,  /* Logical drive number to be mounted/unmounted */
     BYTE opt            /* Mode option 0:Do not mount (delayed mount), 1:Mount immediately */
 )
@@ -3221,21 +3221,33 @@ FRESULT f_mount (
         cfs->fs_type = 0;               /* Clear old fs object */
     }
 
-    if (fs) {
-        fs->fs_type = 0;                /* Clear new fs object */
+    fs->fs_type = 0;                    /* Clear new fs object */
 #if _FS_REENTRANT                       /* Create sync object for the new volume */
-        if (!ff_cre_syncobj((BYTE)vol, &fs->sobj)) return FR_INT_ERR;
+    if (!ff_cre_syncobj((BYTE)vol, &fs->sobj)) return FR_INT_ERR;
 #endif
-    }
     FatFs[vol] = fs;                    /* Register new fs object */
 
-    if (!fs || opt != 1) return FR_OK;  /* Do not mount now, it will be mounted later */
+    if (opt != 1) return FR_OK;         /* Do not mount now, it will be mounted later */
 
     res = find_volume(&path, &fs, 0);   /* Force mounted the volume */
     LEAVE_FF(fs, res);
 }
 
 
+FRESULT f_umount (
+    FATFS* fs                   /* Pointer to the file system object to unmount */
+)
+{
+#if _FS_LOCK
+    clear_lock(fs);
+#endif
+#if _FS_REENTRANT               /* Discard sync object of the current volume */
+    if (!ff_del_syncobj(fs->sobj)) return FR_INT_ERR;
+#endif
+    fs->fs_type = 0;            /* Clear old fs object */
+
+    return FR_OK;
+}
 
 
 /*-----------------------------------------------------------------------*/
