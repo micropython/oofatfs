7571bf6a85241524aa784e30efcc23ed888607fc
Damien George <damien.p.george@gmail.com>
2016-03-07 14:10:14 +0000

    src: Remove get_ldnumber, CurrVol and f_chdrive.

--- src/ff.c
+++ src/ff.c
@@ -435,10 +435,6 @@ typedef struct {
 #endif
 static WORD Fsid;                   /* Filesystem mount ID */
 
-#if FF_FS_RPATH != 0
-static BYTE CurrVol;                /* Current drive */
-#endif
-
 #if FF_FS_LOCK != 0
 static FILESEM Files[FF_FS_LOCK];   /* Open object lock semaphores */
 #endif
@@ -3102,80 +3098,6 @@ static FRESULT follow_path (    /* FR_OK(0): successful, !=0: error code */
 
 
 /*-----------------------------------------------------------------------*/
-/* Get logical drive number from path name                               */
-/*-----------------------------------------------------------------------*/
-
-static int get_ldnumber (   /* Returns logical drive number (-1:invalid drive number or null pointer) */
-    const TCHAR** path      /* Pointer to pointer to the path name */
-)
-{
-    const TCHAR *tp, *tt;
-    TCHAR tc;
-    int i, vol = -1;
-#if FF_STR_VOLUME_ID        /* Find string volume ID */
-    const char *sp;
-    char c;
-#endif
-
-    tt = tp = *path;
-    if (!tp) return vol;    /* Invalid path name? */
-    do tc = *tt++; while ((UINT)tc >= (FF_USE_LFN ? ' ' : '!') && tc != ':');   /* Find a colon in the path */
-
-    if (tc == ':') {    /* DOS/Windows style volume ID? */
-        i = FF_VOLUMES;
-        if (IsDigit(*tp) && tp + 2 == tt) { /* Is there a numeric volume ID + colon? */
-            i = (int)*tp - '0'; /* Get the LD number */
-        }
-#if FF_STR_VOLUME_ID == 1   /* Arbitrary string is enabled */
-        else {
-            i = 0;
-            do {
-                sp = VolumeStr[i]; tp = *path;  /* This string volume ID and path name */
-                do {    /* Compare the volume ID with path name */
-                    c = *sp++; tc = *tp++;
-                    if (IsLower(c)) c -= 0x20;
-                    if (IsLower(tc)) tc -= 0x20;
-                } while (c && (TCHAR)c == tc);
-            } while ((c || tp != tt) && ++i < FF_VOLUMES);  /* Repeat for each id until pattern match */
-        }
-#endif
-        if (i < FF_VOLUMES) {   /* If a volume ID is found, get the drive number and strip it */
-            vol = i;        /* Drive number */
-            *path = tt;     /* Snip the drive prefix off */
-        }
-        return vol;
-    }
-#if FF_STR_VOLUME_ID == 2       /* Unix style volume ID is enabled */
-    if (*tp == '/') {
-        i = 0;
-        do {
-            sp = VolumeStr[i]; tp = *path;  /* This string volume ID and path name */
-            do {    /* Compare the volume ID with path name */
-                c = *sp++; tc = *(++tp);
-                if (IsLower(c)) c -= 0x20;
-                if (IsLower(tc)) tc -= 0x20;
-            } while (c && (TCHAR)c == tc);
-        } while ((c || (tc != '/' && (UINT)tc >= (FF_USE_LFN ? ' ' : '!'))) && ++i < FF_VOLUMES);   /* Repeat for each ID until pattern match */
-        if (i < FF_VOLUMES) {   /* If a volume ID is found, get the drive number and strip it */
-            vol = i;        /* Drive number */
-            *path = tp;     /* Snip the drive prefix off */
-            return vol;
-        }
-    }
-#endif
-    /* No drive prefix is found */
-#if FF_FS_RPATH != 0
-    vol = CurrVol;  /* Default drive is current drive */
-#else
-    vol = 0;        /* Default drive is 0 */
-#endif
-    return vol;     /* Return the default drive */
-}
-
-
-
-
-/*-----------------------------------------------------------------------*/
 /* Load a sector and check if it is an FAT VBR                           */
 /*-----------------------------------------------------------------------*/
 
@@ -4042,23 +3964,6 @@ FRESULT f_close (
 /*-----------------------------------------------------------------------*/
 /* Change Current Directory or Current Drive, Get Current Directory      */
 /*-----------------------------------------------------------------------*/
-
-FRESULT f_chdrive (
-    const TCHAR* path       /* Drive number to set */
-)
-{
-    int vol;
-
-
-    /* Get logical drive number */
-    vol = get_ldnumber(&path);
-    if (vol < 0) return FR_INVALID_DRIVE;
-    CurrVol = (BYTE)vol;    /* Set it as current volume */
-
-    return FR_OK;
-}
-
-
 
 FRESULT f_chdir (
     FATFS *fs,
