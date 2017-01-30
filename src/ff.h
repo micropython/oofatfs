51d88acfabb92ab1d6ea53e8e356733be6c4adab
Damien George <damien.p.george@gmail.com>
2017-01-30 12:03:54 +1100

    src: Make ff_cre_syncobj take a FATFS* as first arg, instead of BYTE.

--- src/ff.h
+++ src/ff.h
@@ -327,7 +327,7 @@ void ff_memfree (void* mblock);         /* Free memory block */
 
 /* Sync functions */
 #if _FS_REENTRANT
-int ff_cre_syncobj (BYTE vol, _SYNC_t* sobj);   /* Create a sync object */
+int ff_cre_syncobj (FATFS *fatfs, _SYNC_t* sobj); /* Create a sync object */
 int ff_req_grant (_SYNC_t sobj);                /* Lock sync object */
 void ff_rel_grant (_SYNC_t sobj);               /* Unlock sync object */
 int ff_del_syncobj (_SYNC_t sobj);              /* Delete a sync object */
